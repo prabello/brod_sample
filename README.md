95f2db8440f1978251d4d2d9627ebdb3cb6108da
Pedro Rabello <pedro.rabello@podium.com>
2020-05-02 10:34:21 -0300

    Project creation

---
+++ README.md
@@ -0,0 +1,21 @@
+# BrodSample
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `brod_sample` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:brod_sample, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/brod_sample](https://hexdocs.pm/brod_sample).
+
---
+++ lib/brod_sample/application.ex
@@ -0,0 +1,19 @@
+defmodule BrodSample.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: BrodSample.Worker.start_link(arg)
+      # {BrodSample.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: BrodSample.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
