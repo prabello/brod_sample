36a1a6cdc3eb7443da7a9bfc1074132b10ff14b9
Pedro Rabello <pedro.rabello@podium.com>
2020-09-03 15:12:12 -0300

    delete topic sample

--- lib/brod_sample/crete_topic.ex
+++ lib/brod_sample/crete_topic.ex
@@ -6,20 +6,34 @@ defmodule BrodSample.CreateTopic do
         num_partitions: 6,
         replica_assignment: [],
         replication_factor: 1,
-        topic: "reviews.snapshots"
+        topic: "test_topic"
       }
     ]
 
+    kafka_url = [
+      {System.get_env("KAFKA_BOOTSTRAP_SERVERS", "localhost"), 9092}
+    ]
+
     :brod.create_topics(
-      ["kafka-default.dev.podium-dev.com": 19092],
+      # ["localhost": 19092],
+      kafka_url,
       topic_config,
       %{timeout: 1_000}
     )
 
     # :brod.create_topics(
-    #   ["kafka-default.dev.podium-dev.com": 19092],
+    #   ["localhost": 19092],
     #   ["test_create_topic"],
     #   %{timeout: 1_000}
     # )
+  end
+
+  @topics ["some.topic", "reviews.attributions", "reviews.listings", "reviews.snapshots"]
+  def delete() do
+    :brod.delete_topics(
+      [localhost: 9092],
+      @topics,
+      10_000
+    )
   end
 end
